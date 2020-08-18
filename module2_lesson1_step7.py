24566ae4c9ff06528c7dd734f16c10fc7dbdc2ab
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-08-18 15:19:17 +0300

    поиск с помощью get_attribute

---
+++ module2_lesson1_step7.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+link = "http://suninjuly.github.io/get_attribute.html"
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    input2 = browser.find_element_by_css_selector("#robotCheckbox")
+    input2.click()
+
+    input3 = browser.find_element_by_css_selector("#robotsRule")
+    input3.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 10 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
+
+
